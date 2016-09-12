29d7815b558a44d63eb6e7f6b3fb343ecd836a0d
Michael Seemann <seemann@mseemann.de>
2016-09-12 14:17:14 +0200

    wip: basic project structure - tests

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const path = require('path');
+
+// Register TS compilation.
+require('ts-node').register({
+  project: path.join(__dirname, 'tools/gulp')
+});
+
+require('./tools/gulp/gulpfile');
---
+++ tools/gulp/tasks/test.ts
@@ -0,0 +1,12 @@
+import * as gulp from 'gulp';
+//const karma = require('karma');
+import * as path from 'path';
+
+gulp.task('test', [], (done: () => void) => {
+    // new karma.Server({
+    //     configFile: path.join(PROJECT_ROOT, 'test/karma.conf.js')
+    // }, done).start();
+
+
+    done();
+});
