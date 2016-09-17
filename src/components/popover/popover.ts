1636b1366c7a69683d4ae5c559b1d0914aa43664
tb <t.bak@selleo.com>
2016-09-17 14:59:36 +0200

    Init demo-app

--- src/components/popover/popover.ts
+++ src/components/popover/popover.ts
@@ -1,5 +1,10 @@
 import {
     Component,
+    Input,
+    AfterViewInit,
+    ViewChild,
+    ElementRef,
+    Renderer,
     ModuleWithProviders,
     NgModule,
     ViewEncapsulation,
@@ -8,17 +13,68 @@ import {
 
 
 @Component({
-    moduleId: module.id,
+    // FIXME angular-cli errors with
+    // url_resolver.js: Uncaught TypeError: uri.match is not a function
+    // this could be fixed with http://stackoverflow.com/a/39346902/478584
+    // but I dont know how to configure webpack preloaders in latest angular-cli
+    // moduleId: module.id,
     selector: 'mdl-popover',
     host: {
         '[class.mdl-popover__container]': 'true'
     },
     templateUrl: 'popover.html',
-    styleUrls: ['popover.css'],
+    styleUrls: ['popover.scss'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdlPopoverComponent {
+export class MdlPopoverComponent implements AfterViewInit {
+    @Input('hide-on-click') public hideOnClick: boolean = false;
+    @Input('width') public width: string = '';
+
+    @ViewChild('container') private containerChild: ElementRef;
+    private container: HTMLElement;
+    private isVisible   = false;
+
+    constructor(private renderer: Renderer) {}
+
+    public ngAfterViewInit() {
+        this.container    = this.containerChild.nativeElement;
+
+        // Add a click listener to the document, to close the popover.
+        var callback = (event) => {
+            if (this.isVisible && (this.hideOnClick || !this.container.contains(event.target))) {
+                this.hide();
+            }
+        };
+        this.renderer.listenGlobal('window', 'click', callback);
+        this.renderer.listenGlobal('window', 'touchstart', callback);
+    }
+
+    public toggle(event: Event) {
+        if (this.isVisible) {
+            this.hide();
+        } else {
+            this.hideAllPopovers();
+            this.show(event);
+        }
+    }
+
+    public hide() {
+        this.container.classList.remove('is-visible');
+        this.isVisible = false;
+    }
+
+    public hideAllPopovers() {
+        [].map.call(document.querySelectorAll('.mdl-popover__container.is-visible'), function(el) {
+            el.classList.remove('is-visible');
+        });
+    }
+
+    public show(event) {
+        event.stopPropagation();
+        this.container.classList.add('is-visible');
+        this.isVisible = true;
+    }
 }
 
 
