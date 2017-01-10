b25723f085f3df7c73f6c42b901667b3c4d49d46
Abdul Qadir <abdulqadir93@outlook.com>
2017-01-10 23:20:44 +0500

    initial implementation for expansion panel

---
+++ src/components/expansion-panel/expansion-panel.component.ts
@@ -0,0 +1,211 @@
+import {
+  Component,
+  NgModule,
+  ModuleWithProviders,
+  AfterContentInit,
+  ContentChild,
+  ContentChildren,
+  EventEmitter,
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  Output,
+  Input,
+  QueryList
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MdlModule } from 'angular2-mdl';
+
+@Component({
+  selector: 'mdl-expansion-panel-header',
+  template: `
+    <ng-content></ng-content>
+    <div class="mdl-expansion-panel__header--expand-icon" (click)="onClick()">
+      <mdl-icon *ngIf="!isExpanded">expand_more</mdl-icon>
+      <mdl-icon *ngIf="isExpanded">expand_less</mdl-icon>
+    </div>
+  `,
+  host: {
+    '[class.mdl-expansion-panel__header]': 'true'
+  }
+})
+export class MdlExpansionPanelHeaderComponent {
+  isExpanded: boolean = false;
+  onChange: EventEmitter<null> = new EventEmitter<null>();
+
+  onClick() {
+    this.onChange.emit();
+  }
+}
+
+@Component({
+  selector: 'mdl-expansion-panel-header-list-content',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel__header--list-content]': 'true'
+  }
+})
+export class MdlExpansionPanelHeaderListContentComponent { }
+
+@Component({
+  selector: 'mdl-expansion-panel-header-secondary-content',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel__header--secondary-content]': 'true'
+  }
+})
+export class MdlExpansionPanelHeaderSecondaryContentComponent { }
+
+@Component({
+  selector: 'mdl-expansion-panel-content',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel__content]': 'true',
+    '[@isExpanded]': 'isExpanded'
+  },
+  animations: [
+    trigger('isExpanded', [
+      state('true', style({ height: '*' })),
+      state('false', style({ height: '0px' })),
+      transition('* => *', animate('250ms ease-in'))
+    ])
+  ]
+})
+export class MdlExpansionPanelContentComponent {
+  isExpanded: boolean = false;
+}
+
+@Component({
+  selector: 'mdl-expansion-panel-body',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel__content--body]': 'true'
+  }
+})
+export class MdlExpansionPanelBodyComponent { }
+
+@Component({
+  selector: 'mdl-expansion-panel-footer',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel__content--footer]': 'true'
+  }
+})
+export class MdlExpansionPanelFooterComponent { }
+
+@Component({
+  selector: 'mdl-expansion-panel',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel]': 'true',
+    '[class.expanded]': 'isExpanded',
+    '[class.disabled]': 'disabled'
+  }
+})
+export class MdlExpansionPanelComponent implements AfterContentInit {
+  @ContentChild(MdlExpansionPanelHeaderComponent) header: MdlExpansionPanelHeaderComponent;
+  @ContentChild(MdlExpansionPanelContentComponent) content: MdlExpansionPanelContentComponent;
+  @Input() disabled: boolean = false;
+  @Output() onChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  isExpanded: boolean = false;
+
+  ngAfterContentInit() {
+    this.header.onChange.subscribe(() => {
+      if (!this.disabled) this._toggle(!this.isExpanded);
+    });
+  }
+
+  toggle() {
+    this._toggle(!this.isExpanded);
+  }
+
+  expand() {
+    this._toggle(true);
+  }
+
+  collapse() {
+    this._toggle(false);
+  }
+
+  _toggle(isExpanded: boolean) {
+    this.isExpanded = isExpanded;
+    this.content.isExpanded = isExpanded;
+    this.header.isExpanded = isExpanded;
+    this.onChange.emit(isExpanded);
+  }
+
+  disableToggle() {
+    this.disabled = false;
+  }
+
+  enableToggle() {
+    this.disabled = true;
+  }
+}
+
+@Component({
+  selector: '<mdl-expansion-panel-group></mdl-expansion-panel-group>',
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class.mdl-expansion-panel-group]': 'true'
+  }
+})
+export class MdlExpansionPanelGroupComponent implements AfterContentInit {
+  @ContentChildren(MdlExpansionPanelComponent) panels: QueryList<MdlExpansionPanelComponent>;
+  expandedIndex: number = -1;
+
+  ngAfterContentInit() {
+    /**
+     * Expand the panel and collapse previously
+     * expanded panel. Save the expanded panel.
+     */
+    this.panels.forEach((panel, i) => {
+      panel.onChange.subscribe((isExpanded: boolean) => {
+        if (isExpanded) {
+          if (i !== this.expandedIndex && this.expandedIndex >= 0) {
+            this.panels.toArray()[this.expandedIndex].collapse();
+          }
+          this.expandedIndex = i;
+        }
+        if (!isExpanded && i === this.expandedIndex) {
+          this.expandedIndex = -1;
+        }
+      });
+    });
+  }
+
+  getExpanded(): number {
+    return this.expandedIndex;
+  }
+
+  getPanel(index: number): MdlExpansionPanelComponent {
+    return this.panels.toArray()[index];
+  }
+}
+
+const MDL_EXPANSION_PANEL_DIRECTIVES = [
+  MdlExpansionPanelGroupComponent,
+  MdlExpansionPanelComponent,
+  MdlExpansionPanelHeaderComponent,
+  MdlExpansionPanelHeaderListContentComponent,
+  MdlExpansionPanelHeaderSecondaryContentComponent,
+  MdlExpansionPanelContentComponent,
+  MdlExpansionPanelBodyComponent,
+  MdlExpansionPanelFooterComponent
+];
+
+@NgModule({
+  imports: [CommonModule, MdlModule],
+  exports: MDL_EXPANSION_PANEL_DIRECTIVES,
+  declarations: MDL_EXPANSION_PANEL_DIRECTIVES,
+})
+export class MdlExpansionPanelModule {
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: MdlExpansionPanelModule,
+      providers: []
+    };
+  }
+}
