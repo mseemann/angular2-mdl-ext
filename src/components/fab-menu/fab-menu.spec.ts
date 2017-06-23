41c29f0303e66aceb6a1c40edc78ac282c607ccf
david <davidkony@hotmail.com>
2017-06-23 14:49:18 +0200

    test for expand and collapse the popover

--- src/components/fab-menu/fab-menu.spec.ts
+++ src/components/fab-menu/fab-menu.spec.ts
@@ -1,8 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MdlFabMenuComponent } from './fab-menu';
+import {MdlFabMenuComponent, MdlFabMenuModule} from './fab-menu';
 import { CommonModule } from '@angular/common';
-import { MdlPopoverModule } from '../popover/popover';
-import { MdlIconModule } from '@angular-mdl/core';
+import {MdlPopoverModule, MdlPopoverComponent} from '../popover/popover';
+import {MdlIconModule, MdlModule} from '@angular-mdl/core';
+import {Component, DebugElement, ComponentRef} from '@angular/core'
+import {MdlFabMenuItemComponent} from './fab-menu-item'
+import {By} from '@angular/platform-browser'
 
 
 describe('MdlFabMenuComponent', () => {
@@ -14,10 +17,11 @@ describe('MdlFabMenuComponent', () => {
         TestBed.configureTestingModule({
             imports: [
                 CommonModule,
-                MdlPopoverModule,
-                MdlIconModule
+                MdlModule,
+                MdlPopoverModule.forRoot()
             ],
-            declarations: [MdlFabMenuComponent],
+            declarations: [MdlFabMenuComponent
+            ],
             providers: [
             ]
         });
@@ -28,7 +32,146 @@ describe('MdlFabMenuComponent', () => {
         });
     }));
 
+    afterEach(()=>{fixture=null})
+
     it('should instantiate the component', async(() => {
         expect(fixture).toBeDefined();
     }));
+
+    it('should have the menu collapsed after the page loads', async(() => {
+        let popoverComponent = fixture.debugElement.query(By.css('.mdl-popover'))
+        let popoverComponentInstance = popoverComponent.componentInstance;
+
+        expect(popoverComponentInstance.isVisible).toBeFalsy();
+
+    }));
+
+    it('should have the menu expanded after the user click once on the FAB', async(() => {
+        let buttonComponent = fixture.debugElement.query(By.css('.mdl-button'))
+
+        let popoverComponent = fixture.debugElement.query(By.css('.mdl-popover'))
+        let popoverComponentInstance = popoverComponent.componentInstance;
+
+      spyOn(popoverComponentInstance, 'toggle').and.callThrough();
+      spyOn(popoverComponentInstance, 'show').and.callThrough();
+
+        buttonComponent.nativeElement.click();
+
+        expect(popoverComponentInstance.show)
+            .toHaveBeenCalled();
+
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+
+            expect(popoverComponentInstance.isVisible).toBeTruthy();
+
+        });
+
+
+
+    }));
+
+    it('should have the menu collapsed after the user click on one item', async(() => {
+        let buttonComponent = fixture.debugElement.query(By.css('.mdl-button'))
+
+        let popoverComponent = fixture.debugElement.query(By.css('.mdl-popover'))
+        let popoverComponentInstance = popoverComponent.componentInstance;
+
+        spyOn(popoverComponentInstance, 'toggle').and.callThrough();
+        spyOn(popoverComponentInstance, 'show').and.callThrough();
+
+        buttonComponent.nativeElement.click();
+
+        expect(popoverComponentInstance.show)
+            .toHaveBeenCalled();
+
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+
+            expect(popoverComponentInstance.isVisible).toBeTruthy();
+
+        });
+
+
+
+    }));
+
+
+    it('should toggle popover on button click', async(() => {
+
+        let popoverComponent = fixture.debugElement.query(By.css('.mdl-popover'))
+
+        let popoverNativeElement = popoverComponent.nativeElement;
+
+        let buttonNativeElement = fixture.debugElement.query(By.css('.mdl-button')).nativeElement;
+
+        let popoverComponentInstance = popoverComponent.componentInstance;
+
+        expect(popoverComponentInstance.isVisible)
+            .toEqual(false, 'isVisible is not false');
+
+        expect(popoverNativeElement.classList.contains('is-visible'))
+            .toBe(false, 'did has css class is-visible');
+
+        spyOn(popoverComponentInstance, 'toggle').and.callThrough();
+
+        spyOn(popoverComponentInstance, 'hideAllPopovers').and.callThrough();
+
+        spyOn(popoverComponentInstance, 'updateDirection').and.callThrough();
+
+        buttonNativeElement.click();
+
+        expect(popoverComponentInstance.toggle).toHaveBeenCalled();
+
+        expect(popoverComponentInstance.hideAllPopovers).toHaveBeenCalled();
+
+        expect(popoverComponentInstance.updateDirection).toHaveBeenCalled();
+
+        expect(popoverComponentInstance.isVisible)
+            .toEqual(true, 'toggle did not update isVisible to true');
+
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+
+            expect(popoverNativeElement.classList.contains('is-visible'))
+                .toBe(true, 'did not has css class is-visible');
+
+        });
+
+    }));
+
 });
+
+@Component({
+    template: `
+    <mdl-fab-menu #mainFabMenu>
+        <mdl-fab-menu-item
+                [fabMenu]="mainFabMenu"
+                icon="note_add"
+                label="make a note"
+                (menu-clicked)="alert('we need to do something here')">
+        </mdl-fab-menu-item>
+        <mdl-fab-menu-item
+                [fabMenu]="mainFabMenu"
+                icon="refresh"
+                label="refresh"
+                (menu-clicked)="alert('user elected to refresh!')">
+        </mdl-fab-menu-item>
+    </mdl-fab-menu>  
+    <mdl-fab-menu #mainFabMenu2 [alwaysShowTooltips]="true">
+        <mdl-fab-menu-item
+                [fabMenu]="mainFabMenu2"
+                icon="note_add"
+                label="make a note"
+                (menu-clicked)="alert('we need to do something here')">
+        </mdl-fab-menu-item>
+        <mdl-fab-menu-item
+                [fabMenu]="mainFabMenu2"
+                icon="refresh"
+                label="refresh"
+                (menu-clicked)="alert('user elected to refresh!')">
+        </mdl-fab-menu-item>
+    </mdl-fab-menu>  
+    `
+})
+class TestMdlFabMenuComponent {}
