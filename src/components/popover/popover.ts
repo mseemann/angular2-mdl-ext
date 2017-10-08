955abb5b864712d2b17de27951c8fd04566cc1b8
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2017-10-08 22:38:22 +0300

    -added ability to user specify popover position using [mdl-popover-position] attribute -added some examples to e2e app demo

--- src/components/popover/popover.ts
+++ src/components/popover/popover.ts
@@ -14,6 +14,7 @@ import {
     ViewEncapsulation,
 } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
+import { MdlButtonComponent } from '@angular-mdl/core';
 
 @Injectable()
 export class MdlPopoverRegistry {
@@ -36,11 +37,70 @@ export class MdlPopoverRegistry {
     }
 
     public hideAllExcept(popoverComponent: MdlPopoverComponent) {
-        this.popoverComponents.forEach( (component) => {
+        this.popoverComponents.forEach((component) => {
             if (component !== popoverComponent) {
                 component.hide();
             }
         });
+    }
+}
+
+const BOTTOM_LEFT = 'bottom-left'; // Below the element, aligned to its left.
+const BOTTOM_RIGHT = 'bottom-right'; // Below the element, aligned to its right.
+const TOP_LEFT = 'top-left'; // Above the element, aligned to its left.
+const TOP_RIGHT = 'top-right'; // Above the element, aligned to its right.
+
+interface IPositionCoordinates {
+    left: number;
+    top: number;
+}
+
+@Injectable()
+export class PopupPositionService {
+    public applyCoordinates(coordinates: IPositionCoordinates, elementStyle: CSSStyleDeclaration) {
+        if (!coordinates) {
+            return;
+        }
+
+        elementStyle.right =
+            elementStyle.bottom = '';
+
+        for (const propertyName in coordinates) {
+            if (!coordinates.hasOwnProperty(propertyName)) {
+                continue;
+            }
+            const value = coordinates[propertyName];
+            elementStyle[propertyName] = value ? value + 'px' : '';
+        }
+    }
+
+    public calculateCoordinates(forElement: HTMLElement, popoverElement: HTMLElement, position: string): IPositionCoordinates {
+        if (!forElement || !position) {
+            return null;
+        }
+
+        switch (position) {
+            case BOTTOM_RIGHT:
+                return {
+                    top: forElement.offsetTop + forElement.offsetHeight,
+                    left: forElement.offsetLeft + forElement.offsetWidth - popoverElement.offsetWidth
+                };
+            case BOTTOM_LEFT:
+                return {
+                    top: forElement.offsetTop + forElement.offsetHeight,
+                    left: forElement.offsetLeft
+                };
+            case TOP_LEFT:
+                return {
+                    top: forElement.offsetTop - popoverElement.offsetHeight,
+                    left: forElement.offsetLeft
+                };
+            case TOP_RIGHT:
+                return {
+                    top: forElement.offsetTop - popoverElement.offsetHeight,
+                    left: forElement.offsetLeft + forElement.offsetWidth - popoverElement.offsetWidth
+                };
+        }
     }
 }
 
@@ -55,6 +115,7 @@ export class MdlPopoverRegistry {
 })
 export class MdlPopoverComponent {
     @Input('hide-on-click') public hideOnClick: boolean = false;
+    @Input('mdl-popover-position') public position: string;
     @Output() onShow: EventEmitter<any> = new EventEmitter();
     @Output() onHide: EventEmitter<any> = new EventEmitter();
     @HostBinding('class.is-visible') public isVisible = false;
@@ -66,20 +127,21 @@ export class MdlPopoverComponent {
     }
 
     constructor(private changeDetectionRef: ChangeDetectorRef,
-                public elementRef: ElementRef,
-                private popoverRegistry: MdlPopoverRegistry) {
+        public elementRef: ElementRef,
+        private popoverRegistry: MdlPopoverRegistry,
+        private popupPositionService: PopupPositionService) {
         this.popoverRegistry.add(this);
     }
 
     public ngOnDestroy() {
-       this.popoverRegistry.remove(this);
+        this.popoverRegistry.remove(this);
     }
 
-    public toggle(event: Event) {
+    public toggle(event: Event, forElement: any = null) {
         if (this.isVisible) {
             this.hide();
         } else {
-            this.show(event);
+            this.show(event, forElement);
         }
     }
 
@@ -95,41 +157,62 @@ export class MdlPopoverComponent {
         this.popoverRegistry.hideAllExcept(this);
     }
 
-    public show(event: Event) {
+    public show(event: Event, forElement: any = null) {
         this.hideAllPopovers();
         event.stopPropagation();
         if (!this.isVisible) {
             this.onShow.emit(null);
             this.isVisible = true;
-            this.updateDirection(event);
+            this.updateDirection(event, forElement);
         }
     }
 
-    private updateDirection(event: Event) {
-        const nativeEl = this.elementRef.nativeElement;
+    private updateDirection(event: Event, forElement: any = null) {
+        const popoverElement = this.elementRef.nativeElement;
         const targetRect = (<HTMLElement>event.target).getBoundingClientRect();
         const viewHeight = window.innerHeight;
 
         setTimeout(() => {
-            let height = nativeEl.offsetHeight;
+            if (forElement && this.position) {
+                const forHtmlElement = this.getHtmlElement(forElement);
+                const coordinates = this.popupPositionService.calculateCoordinates(forHtmlElement, popoverElement, this.position);
+                this.popupPositionService.applyCoordinates(coordinates, popoverElement.style);
+
+                this.changeDetectionRef.markForCheck();
+                // since we have user specified directions maybe it's better to let user to decide when and where the popup should be directed at?
+                // my point: we should not use the following code with "auto dirrection up"
+                return;
+            }
+
+            const height = popoverElement.offsetHeight;
             if (height) {
-                const spaceAvailable = {
-                    top: targetRect.top,
-                    bottom: viewHeight - targetRect.bottom
-                };
-                this.directionUp = spaceAvailable.bottom < height;
+                const bottomSpaceAvailable = viewHeight - targetRect.bottom
+                this.directionUp = bottomSpaceAvailable < height;
                 this.changeDetectionRef.markForCheck();
             }
         });
     }
-}
 
+    private getHtmlElement(forElement: any): HTMLElement {
+        if (forElement instanceof MdlButtonComponent) {
+            const buttonComponent = <MdlButtonComponent>forElement;
+            return buttonComponent.elementRef.nativeElement;
+        }
+
+        if (forElement instanceof ElementRef) {
+            const elementRef = <ElementRef>forElement;
+            return elementRef.nativeElement;
+        }
+
+        return forElement;
+    }
+}
 
 @NgModule({
     imports: [],
     exports: [MdlPopoverComponent],
     declarations: [MdlPopoverComponent],
-    providers: [MdlPopoverRegistry],
+    providers: [MdlPopoverRegistry, PopupPositionService],
 })
 export class MdlPopoverModule {
     static forRoot(): ModuleWithProviders {
